d28f021551a9d1ca792f386fab3ed98c11a2d8f3
AishwaryaSudarshan <aishwaryasudarshan18@gmail.com>
2023-01-13 16:20:42 -0600

    update file names

--- src/Navbar.js
+++ src/Navbar.js
@@ -8,18 +8,18 @@ function Navbar() {
                 <div className="App-contacts">
                     <div className="App-icons">
                         <a href="https://github.com/AishwaryaSudarshan">
-                            <img src='/github.png' alt='github'></img>
+                            <img src='github.png' alt='github'></img>
                         </a>
                         <a href="mailto:aishwaryasudarshan18@gmail.com">
-                            <img src='/email.png' alt='email'></img>
+                            <img src='email.png' alt='email'></img>
                         </a>
                         <a href="https://www.linkedin.com/in/aishwarya-sudarshan/">
-                            <img src='/linkedin.png' alt='linkedin'></img>
+                            <img src='linkedin.png' alt='linkedin'></img>
                         </a>
                     </div>
                 </div>
                 <div className="App-resume">
-                    <a href="/Resume.pdf">
+                    <a href="Resume.pdf">
                         <div className='App-resume-border'>
                             <resume>Resume</resume>
                         </div>
