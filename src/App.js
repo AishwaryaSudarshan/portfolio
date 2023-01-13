0952c74b6e6a5fa04331c8d6891931308e22c4fb
AishwaryaSudarshan <aishwaryasudarshan18@gmail.com>
2023-01-13 13:47:16 -0600

    create app.js

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Navbar from './Navbar';
+import Header from './Header';
+import Projects from './Projects';
+import Experience from './Experience';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <Projects />
+      <Experience />
+    </>
+  );
+}
+
+export default App;
