f22ed96caaa73908d9653c3373926f90d6caf3fc
AishwaryaSudarshan <aishwaryasudarshan18@gmail.com>
2023-01-13 13:47:32 -0600

    create header.js

---
+++ src/Header.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function Header() {
+    return (
+      <html>
+        <div className="App-body">
+          <div className="App-section">
+            <div className="App-content">
+                <div className='App-profile-pic'>
+                  <img src="/profile.jpeg" alt='profile' />
+                </div>
+                <div className='App-header-text'>
+                  <h2>Aishwarya Sudarshan</h2>
+                  <p>B.S Computer Science at UT Dallas</p>
+              </div>
+            </div>
+        </div>
+        </div>
+      </html>
+    );
+  }
+  export default Header; 
